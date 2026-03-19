b669792941f93e59b83868e41def8e11e6af1031
julie634 <julie@foundsm.com>
2026-03-19 01:17:44 +0000

    Add Vercel Analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import App from "./App";
 import "./index.css";
+
+inject();
 
 if (!window.location.hash) {
   window.location.hash = "#/";
